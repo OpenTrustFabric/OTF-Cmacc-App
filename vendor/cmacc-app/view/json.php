6c5a211f8f96c8f50e2dca78b0cd4d4d8ae0a620
Md Mahfuzul Islam <mahfuzul.islam@exrade.com>
2021-02-10 12:41:46 +0100

    Initial commit of Commonaccord app repo without the Prose Objects

---
+++ vendor/cmacc-app/view/json.php
@@ -0,0 +1,69 @@
+<?php
+include("header.php");
+
+include("$lib_path/view-tabs.php");
+
+?>
+
+
+<h4>JSON source view - <a href="https://github.com/adonalsium/common_accord/blob/dev/README.md">see technical description from Brown DISP ReadMe</a>:</h4>
+
+<?php 
+
+$text="{<ul type='none'><li>\"edges\" : [ <ul type='none'><li>";
+
+# First the links (edges):
+
+foreach($contents as $n) {
+        list($k, $v) = array_pad( explode ("=", $n, 2), 2, null);
+
+if(preg_match('/\[(.+?)\]/', $v, $matches)) {
+ 
+   if(strlen($k) > 0){
+     $vlink = "\"<a href=$_SERVER[PHP_SELF]?action=json&file=$matches[1]>$matches[1]</a>\"" ;
+     $text = $text . "[ \"$k\" : $vlink ] , "; 
+   }
+   else{ 
+     $vlink = "\"\" : \"<a href=$_SERVER[PHP_SELF]?action=json&file=$matches[1]>$matches[1]</a>\""; 
+     $text = $text . "[ $vlink ] , "; 
+   }
+}
+
+
+}
+
+$text = $text . "ENDEDGES";
+$text = str_replace("ENDEDGES", "<br>]</ul>\"data\" {<ul type='none'><li>", $text);
+
+
+
+
+# Then the Data k/v's.  :
+
+
+foreach($contents as $n) {
+  list($k, $v) = array_pad( explode ("=", $n, 2), 2, null);
+
+if(preg_match('/\[(.+?)\]/', $v, $matches)) {
+
+}
+
+elseif(isset($v)) { 
+$vhtml =  htmlspecialchars(json_encode($v));
+$vhtml = str_replace('\/', '/', $vhtml);
+$vhtml = str_replace('{', '", "', $vhtml);
+$vhtml = str_replace('}', '", "', $vhtml);
+$text= $text .  "\"$k\" : $vhtml ,<br><br>"; 
+}
+  else { 
+}
+}
+
+
+
+$text = $text . "QEND";
+$text = str_replace('QEND', '<br>}</ul></ul>}', $text);
+
+
+echo $text
+?>
---
+++ vendor/cmacc-app/view/source.php
@@ -0,0 +1,57 @@
+
+<?php
+
+
+include("header.php");
+
+
+include("$lib_path/view-tabs.php");
+
+echo "<div class='container'>___<hr><table class='TFtable';>";
+foreach($contents as $n) {
+        list($k, $v) = array_pad( explode ("=", $n, 2), 2, null);
+       
+	//for remote files via http
+        if(preg_match('/\[\?(.+?)\]/', $v, $matches)) {
+	  // to allow comments in the target field.
+	 	  list($v1, $v2) = array_pad( explode ("///", $v, 2), 2, null);
+	  	  $v1=substr($v1, 2, -1);
+	  $v = "<a href=$matches[1]>$v1</a></td><td>$v2";
+        }
+	//for folders (navigation)
+
+	else if(preg_match('/\[(.+?)\\/]/', $v, $matches)) {
+                $v = "<a href=$_SERVER[PHP_SELF]?action=list&file=$matches[1]/>$v</a>";
+	}
+
+	//for local files
+
+        else if(preg_match('/\[(.+?)\]/', $v, $matches)) {
+          // to allow comments in the target field.
+	  list($v1, $v2) = array_pad( explode ("///", $v, 2), 2, null);
+
+	  $v = "<a href=$_SERVER[PHP_SELF]?action=source&file=$matches[1]>$v1</a></td><td>$v2";
+
+        }
+
+       	
+        echo "<tr id=$k>";
+        if(isset($v)) { echo "<th height='10' width='300' style='text-align:right'>$k</th><td width='20'></td><td>$v</td>"; }
+        else { echo "<th height='10' style='text-align:right'></th><td width='20'></td><td>$k</td>"; }
+        echo "</tr>";
+}
+
+?>
+</table>
+
+</div>
+
+
+</div>
+
+
+
+
+</div></div>
+
+</div>
